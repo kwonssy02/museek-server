f6d1f5686018dfa9e602deadbca83b24f1cbb1e8
KHC <kwonssy02@gmail.com>
2015-12-02 00:25:32 +0900

    playlist 조회

--- routes/playlist.js
+++ routes/playlist.js
@@ -25,6 +25,16 @@ const addSongToCurrentPlaylistQuery = ("INSERT INTO PlaylistSongs               
                                         );");
 
 
+const getPlaylistsByIdURL = ("/getPlaylistsById/:userId");
+const selectPlaylistsByIdQuery = ("SELECT playlistId, name, userId, regiDate FROM Playlist where userId = ? ORDER BY playlistId asc");
+const selectPlaylistSongsQuery = ("SELECT B.playlistId playlistId, B.songId songId, B.seq seq, C.title title, C.singer singer, C.duration duration \
+                                FROM Playlist A INNER JOIN PlaylistSongs B              \
+                                ON A.playlistId = B.playlistId                          \
+                                INNER JOIN Song C                                       \
+                                ON B.songId = C.songId                                  \
+                                where userId = ?                                        \
+                                ORDER BY B.playlistId asc, B.seq desc");
+
 router.post(addSongToCurrentPlaylistURL, addSongToCurrentPlaylist);
 function addSongToCurrentPlaylist(req, res, next) {
         var userId = req.body.userId;
@@ -47,6 +57,36 @@ function addSongToCurrentPlaylist(req, res, next) {
 
         });
 
+}
+
+router.get(getPlaylistsByIdURL, getPlaylistsById);
+function getPlaylistsById(req, res, next) {
+        var userId = req.params.userId;
+        var queryParams = [userId];
+        connection.query(selectPlaylistsByIdQuery, queryParams, function(err, playlists, fields) {
+                if(err) {
+                        throw err;
+                }
+                
+                var queryParams2 = [userId];
+                connection.query(selectPlaylistSongsQuery, queryParams2, function(err, songs, fields) {
+                        if(err) {
+                                throw err;
+                        }
+
+                        for(var i=0; i<playlists.length; i++) {
+                                var playlistId = playlists[i].playlistId;
+                                playlists[i]["songs"] = [];
+                                for(var j=0; j<songs.length; j++) {
+                                        if(songs[j].playlistId == playlistId) {
+                                               playlists[i]["songs"].push(songs[j]);
+                                        }
+                                }
+                        }
+                        //playlists
+                        jsonFormat.successRes(res, 'getPlaylistsById', playlists);
+                });
+        });
 }
 
 
